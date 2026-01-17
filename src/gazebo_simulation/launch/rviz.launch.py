8c196dcbcab2d82a477b421c4ad14e1e7d8d9217
Marcelin <lenforiee@gmail.com>
2026-01-17 22:14:02 +0100

    feat: created compopsite launch components with equivalent args

---
+++ src/gazebo_simulation/launch/rviz.launch.py
@@ -0,0 +1,10 @@
+import argparse
+
+def main() -> int:
+    args = argparse.ArgumentParser()
+    args.add_argument("--config-path")
+
+    return 0
+
+if __name__ == "__main__":
+    raise SystemExit(main())
---
+++ src/gazebo_simulation/launch/world_only.launch.py
@@ -0,0 +1,10 @@
+import argparse
+
+def main() -> int:
+    args = argparse.ArgumentParser()
+    args.add_argument("--world-file")
+
+    return 0
+
+if __name__ == "__main__":
+    raise SystemExit(main())

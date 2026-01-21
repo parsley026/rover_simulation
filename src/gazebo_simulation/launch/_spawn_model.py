21f92babf78ab710199322ed5541853428948630
Marcelin <lenforiee@gmail.com>
2026-01-21 13:55:54 +0100

    feat: add basic spawn model

--- src/gazebo_simulation/launch/_spawn_model.py
+++ src/gazebo_simulation/launch/_spawn_model.py
@@ -1,11 +1,93 @@
-import argparse
+from typing import Any
 
-def main() -> int:
-    args = argparse.ArgumentParser()
-    args.add_argument("--model-file")
-    args.add_argument("--pose")
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.actions import OpaqueFunction
+from launch.substitutions import LaunchConfiguration
+from launch.substitutions import PathJoinSubstitution
+from launch.launch_context import LaunchContext
 
-    return 0
+def _check_float(s: str) -> bool:
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
 
-if __name__ == "__main__":
-    raise SystemExit(main())
+def _parse_pose(s: str) -> list[str]:
+    clean_string = s.strip().lower()
+
+    coords = clean_string.split(" ")
+    if (not clean_string
+        or not len(coords) == 3
+        or not _check_float(coords[0])
+        or not _check_float(coords[1])
+        or not _check_float(coords[2])):
+        return ['0', '0', '0.5']
+    
+    return coords
+
+def launch_setup(context: LaunchContext, *_, **__) -> list[Any]:
+
+    model_file = LaunchConfiguration('model_file')
+    pose = LaunchConfiguration('pose')
+
+    model_file_desc = model_file.perform(context)
+    model_coords = _parse_pose(pose.perform(context))
+
+    with open(model_file_desc) as stream:
+        robot_desc = stream.read()
+
+    robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        output='both',
+        parameters=[
+            {'use_sim_time': True},
+            {'robot_description': robot_desc},
+        ]
+    )
+
+    spawn_entity = Node(
+        package="ros_gz_sim",
+        executable="create",
+        arguments=[
+            "-name",
+            "rex",
+            "-topic",
+            "/robot_description",
+            "-x",
+            model_coords[0],
+            "-y",
+            model_coords[1],
+            "-z",
+            model_coords[2],
+        ],
+        output="screen",
+    )
+
+    return [robot_state_publisher, spawn_entity]
+
+
+def generate_launch_description() -> LaunchDescription:
+    pkg_project_description = get_package_share_directory('rover_description')
+    
+    model_file_launch_arg = DeclareLaunchArgument(
+        'model_file',
+        default_value=PathJoinSubstitution([pkg_project_description, 'urdf', 'rex.urdf']),
+        description='Path to URDF model file'
+    )
+
+    pose_launch_arg = DeclareLaunchArgument(
+        'pose',
+        default_value='0 0 0.5',
+        description='XYZ coordinates of the running model'
+    )
+
+    opaque_func = OpaqueFunction(function=launch_setup)
+
+    return LaunchDescription([model_file_launch_arg, pose_launch_arg, opaque_func])
+

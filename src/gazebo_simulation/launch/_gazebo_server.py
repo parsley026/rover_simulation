9536dd917b09fe734f434ace3af552551e40e857
Marcelin <lenforiee@gmail.com>
2026-01-21 12:40:12 +0100

    feat: write a simple gazebo simulation server

--- src/gazebo_simulation/launch/_gazebo_server.py
+++ src/gazebo_simulation/launch/_gazebo_server.py
@@ -1,11 +1,46 @@
-import argparse
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.actions import OpaqueFunction
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch.substitutions import PathJoinSubstitution
 
-def main() -> int:
-    args = argparse.ArgumentParser()
-    args.add_argument("--world-file")
-    args.add_argument("--headless")
+def launch_setup(context, *_, **__):
+    pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim')
 
-    return 0
+    world_file = LaunchConfiguration('world_file')
+    headless = LaunchConfiguration('headless')
+    check_headless = headless.perform(context).strip().lower() in ('1', 'true')
 
-if __name__ == "__main__":
-    raise SystemExit(main())
+
+    gazebo_sim = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            PathJoinSubstitution([pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py'])
+        ),
+        launch_arguments={'gz_args': ['-r ', '-v4 ', '-s ' if check_headless else '', world_file]}.items(),
+    )
+
+    return [gazebo_sim]
+
+
+def generate_launch_description():
+    pkg_project_gazebo = get_package_share_directory('gazebo_simulation')
+
+    world_file_launch_arg = DeclareLaunchArgument(
+        'world_file',
+        default_value=PathJoinSubstitution([pkg_project_gazebo, 'worlds', 'rex.sdf']),
+        description='Path to SDF world file'
+    )
+
+    headless_launch_arg = DeclareLaunchArgument(
+        'headless',
+        default_value='false',
+        description='Run without GUI'
+    )
+
+    opaque_func = OpaqueFunction(function=launch_setup)
+
+    return LaunchDescription([world_file_launch_arg, headless_launch_arg, opaque_func])
+
